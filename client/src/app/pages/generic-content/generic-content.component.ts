50d3d324914d825d5f65ee09c18586c74587060f
Vyacheslav Maron <vmaronmail@gmail.com>
2022-03-07 22:53:03 -0500

    Display styled page body  content

--- client/src/app/pages/generic-content/generic-content.component.ts
+++ client/src/app/pages/generic-content/generic-content.component.ts
@@ -1,18 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {State} from "../../../core/navigation/navigation.state";
+import {select, Store} from "@ngrx/store";
+import {Subject, takeUntil} from "rxjs";
+import {selectNavigation} from "../../../core/navigation/navigation.selectors";
+
 
 @Component({
   selector: 'app-generic-content',
   templateUrl: './generic-content.component.html',
   styleUrls: ['./generic-content.component.scss']
 })
-export class GenericContentComponent implements OnInit {
+export class GenericContentComponent implements OnInit, OnDestroy{
 
   routeUrl: string = '';
+  ngUnsubscribe: Subject<any> = new Subject();
+  loading: boolean = false;
+  pageBody: string = '';
 
   constructor(private readonly route: ActivatedRoute,
-              private router: Router) {
+              private router: Router,
+              private store: Store<State>) {
   }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next(true);
+    this.ngUnsubscribe.complete();
+    }
 
   ngOnInit(): void {
     this.routeUrl = this.router.url;
@@ -20,6 +34,11 @@ export class GenericContentComponent implements OnInit {
       if (val instanceof NavigationEnd) {
         this.routeUrl = this.router.url;
       }
+    });
+
+    this.store.pipe(select(selectNavigation)).pipe(takeUntil(this.ngUnsubscribe)).subscribe((nav) => {
+      this.loading = nav.loading;
+      this.pageBody = nav.pageBody;
     });
   }
 
--- client/src/app/pages/generic-content/generic-content.module.ts
+++ client/src/app/pages/generic-content/generic-content.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { GenericContentRoutingModule } from './generic-content-routing.module';
-import { GenericContentComponent } from './generic-content.component';
+import {GenericContentRoutingModule} from './generic-content-routing.module';
+import {GenericContentComponent} from './generic-content.component';
+import {SafeHtmlPipe} from "./pipes/safe-html.pipe";
 
 
 @NgModule({
   declarations: [
-    GenericContentComponent
+    GenericContentComponent,
+    SafeHtmlPipe
   ],
   imports: [
     CommonModule,
     GenericContentRoutingModule
   ]
 })
-export class GenericContentModule { }
+export class GenericContentModule {
+}
