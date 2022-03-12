5260c6f6a89524fabbf4968f622c5a50265dec25
Vyacheslav Maron <vmaronmail@gmail.com>
2022-03-12 14:22:28 -0500

    Add user menu component

--- client/src/app/components/menu/menu.module.ts
+++ client/src/app/components/menu/menu.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MenuComponent} from './menu.component';
 import {RouterModule} from "@angular/router";
+import {UserMenuComponent} from "./user-menu/user-menu.component";
 
 @NgModule({
-  declarations: [MenuComponent],
+  declarations: [MenuComponent, UserMenuComponent],
   imports: [CommonModule, RouterModule],
   exports: [MenuComponent],
 })
