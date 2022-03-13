3fb7ab6fb3c3bee86a8532b0231d7632f3529917
Vyacheslav Maron <vmaronmail@gmail.com>
2022-03-12 23:25:08 -0500

    Sticky header

--- client/src/app/components/menu/user-menu/user-menu.component.ts
+++ client/src/app/components/menu/user-menu/user-menu.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user-menu',
   template: `
     <!-- Icon -->
-    <a class="text-white opacity-60 hover:opacity-80 focus:opacity-80 mr-4" href="#">
+    <a class="hover:opacity-80 focus:opacity-80 mr-4" href="#">
       <svg
         aria-hidden="true"
         focusable="false"
@@ -21,62 +21,23 @@ import { Component, OnInit } from '@angular/core';
         ></path>
       </svg>
     </a>
-    <div class="dropdown relative">
-      <a
-        class="text-white opacity-60 hover:opacity-80 focus:opacity-80 mr-4 dropdown-toggle hidden-arrow flex items-center"
-        href="#"
-        id="dropdownMenuButton1"
-        role="button"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        <svg
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fas"
-          data-icon="bell"
-          class="w-4"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-        >
-          <path
-            fill="currentColor"
-            d="M224 512c35.32 0 63.97-28.65 63.97-64H160.03c0 35.35 28.65 64 63.97 64zm215.39-149.71c-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84C118.56 68.1 64.08 130.3 64.08 208c0 102.3-36.15 133.53-55.47 154.29-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h383.8c19.12 0 32-15.6 32.1-32 .05-7.55-2.61-15.27-8.61-21.71z"
-          ></path>
-        </svg>
-        <span
-          class="text-white bg-red-700 absolute rounded-full text-xs -mt-2.5 ml-2 py-0 px-1.5"
-        >1</span
-        >
-      </a>
-      <ul
-        class="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none left-auto right-0"
-        aria-labelledby="dropdownMenuButton1"
-      >
-        <li>
-          <a
-            class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-            href="#"
-          >Action</a
-          >
-        </li>
-        <li>
-          <a
-            class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-            href="#"
-          >Another action</a
-          >
-        </li>
-        <li>
-          <a
-            class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-            href="#"
-          >Something else here</a
-          >
-        </li>
-      </ul>
-    </div>
+    <a
+      class="relative align-middle rounded-md focus:outline-none focus:shadow-outline-purple mr-4" href="#">
+      <svg
+        aria-hidden="true"
+        class="w-5 h-5"
+        fill="currentColor"
+        viewBox="0 0 20 20">
+        <path
+          d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z">
+        </path>
+      </svg>
+      <!-- Notification badge -->
+      <span
+        aria-hidden="true"
+        class="absolute top-0 right-0 inline-block w-3 h-3 transform translate-x-1 -translate-y-1 bg-red-600 border-2 border-white rounded-full dark:border-gray-800">
+      </span>
+    </a>
     <div class="dropdown relative">
       <a
         class="dropdown-toggle flex items-center hidden-arrow"
@@ -95,7 +56,7 @@ import { Component, OnInit } from '@angular/core';
         />
       </a>
       <ul
-        class="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none left-auto right-0"
+        class="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left mt-1 hidden m-0 bg-clip-padding border border-gray-100 rounded-md shadow-md left-auto right-0"
         aria-labelledby="dropdownMenuButton2"
       >
         <li>
@@ -123,12 +84,12 @@ import { Component, OnInit } from '@angular/core';
     </div>
     <ng-content></ng-content>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class UserMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
